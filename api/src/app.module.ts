dce41ce44d80e6926a1aed7ad385c5b1dbb07f03
isaias-silva <zackblackmangold@gmail.com>
2024-04-06 14:22:57 -0300

    feat: :sparkles: adicionando modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { RestaurantModule } from './modules/restaurant/restaurant.module';
+import { DisheModule } from './modules/dishe/dishe.module';
+import { MenuModule } from './modules/menu/menu.module';
+import { CategoryModule } from './modules/category/category.module';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RestaurantModule,
+    DisheModule,
+    MenuModule,
+    CategoryModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
